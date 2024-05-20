ea1d7692938fd59af620bc1c506c8a2f28ce2615
Sammm <ntdsam@gmail.com>
2024-05-20 10:12:11 +0700

    create components

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,15 @@
 import { ScrollView, Image, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { Link } from "expo-router";
+import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
+import { CustomButton } from "../components";
 
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -32,8 +33,21 @@ export default function App() {
               resizeMode="contain"
             />
           </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Aora
+          </Text>
+
+          <CustomButton
+            title="Continue with Email"
+            hanldePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 }
