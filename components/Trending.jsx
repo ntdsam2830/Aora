a537424576de6c332f505dd614eb6992b0cd3ffa
Sammm <ntdsam@gmail.com>
2024-06-07 14:07:02 +0700

    Trending

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -8,13 +8,44 @@ import {
 import { useState, React } from "react";
 import * as Animatable from "react-native-animatable";
 
+import { icons } from "../constants";
+
+const zoomIn = {
+  0: {
+    scale: 0.9,
+  },
+  1: {
+    scale: 1,
+  },
+};
+
+const zoomOut = {
+  0: {
+    scale: 1,
+  },
+  1: {
+    scale: 0.9,
+  },
+};
+
+const TrendingItem = (activeItem, item) => {
+  return (
+    <Animatable.View
+      className="mr-5"
+      animation={activeItem === item.$id ? zoomIn : zoomOut}
+      duration={500}
+    ></Animatable.View>
+  );
+};
+
 const Trending = ({ posts }) => {
+  const [activeItem, setActiveItem] = useState(posts[0]);
   return (
     <FlatList
       data={posts}
       keyExtractor={(item) => item.$id}
       renderItem={({ item }) => (
-        <Text className="text-3xl text-white">{item.id}</Text>
+        <TrendingItem activeItem={activeItem} item={item} />
       )}
       horizontal
     />
